9b9f7a0166add0a187b2c3db7806379a3ef25396
jardel.bernardo <jardel.bernardo@alien-group.com>
2026-07-01 04:35:36 +0100

    [NEW] SMART_CONNECTOR: add a base struture for a module

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Smart connector",
+    "description": "Smart Connector Description",
+    "summary": "Smart Connector Summary",
+    "odoo_version": "18.0.0",
+    "version": "1.0.0",
+    "depends": [
+        'base'
+    ],
+
+}
---
+++ models/ir_config.py
@@ -0,0 +1,5 @@
+from odoo import models, fields, api
+
+
+class IrConfigInherit(models.Model):
+    _inherit = 'ir.config'
